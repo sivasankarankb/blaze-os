f8c341757318347a61f2499573182a43bf9ff90d
Sivasankaran K B <kbsiva7@gmail.com>
2025-04-29 19:53:22 +0530

    Update mingw, djgpp scripts for blaze v0.1.1

--- extras/djbuild.bat
+++ extras/djbuild.bat
@@ -1,7 +1,8 @@
 echo off
 cls
 
-rem DJGPP Build Script for Blaze OS v0.1.0
+rem DJGPP Build Script for Blaze OS v0.1.1
+set CFILECOUNT=10
 
 echo 1 of 6 Build bootloader
 
@@ -11,25 +12,27 @@ nasm -f bin -o bin\boot.bin boot.asm
 echo 2 of 6 Build kernel sources
 
 mkdir obj
-echo   1 of 9 idt.asm
+echo   1 of %CFILECOUNT% idt.asm
 nasm -f aout -o obj\idt.o idt.asm
 
-echo   2 of 9 kernel.c
+echo   2 of %CFILECOUNT% kernel.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\kernel.o kernel.c
-echo   3 of 9 irq.c
+echo   3 of %CFILECOUNT% irq.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\irq.o irq.c
-echo   4 of 9 isr.c
+echo   4 of %CFILECOUNT% isr.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\isr.o isr.c
-echo   5 of 9 itext.c
+echo   5 of %CFILECOUNT% itext.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\itext.o itext.c
-echo   6 of 9 memmgr.c
+echo   6 of %CFILECOUNT% memmgr.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\memmgr.o memmgr.c
-echo   7 of 9 pic.c
+echo   7 of %CFILECOUNT% pic.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\pic.o pic.c
-echo   8 of 9 ports.c
+echo   8 of %CFILECOUNT% ports.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\ports.o ports.c
-echo   9 of 9 ps2.c
+echo   9 of %CFILECOUNT% ps2.c
 gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\ps2.o ps2.c
+echo   10 of %CFILECOUNT% power.c
+gcc -c -nostdinc -nostdlib -ffreestanding -march=i386 -fleading-underscore -o obj\power.o power.c
 
 echo 3 of 6 Create library file
 
@@ -41,6 +44,7 @@ ar rs obj\libk.a obj\memmgr.o
 ar rs obj\libk.a obj\pic.o
 ar rs obj\libk.a obj\ports.o
 ar rs obj\libk.a obj\ps2.o
+ar rs obj\libk.a obj\power.o
 
 echo 4 of 6 Create kernel binary
 
